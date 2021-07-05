d25aa22d9da8e5f192d96d5fc220dfec7370a474
Tharana Mayuranga <tharana.maya@gmail.com>
2021-07-05 13:13:09 +0530

    outputting a list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,12 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A test recipe',
       'this test',
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.simplyrecipes.om%2Frecipes%2Fgrilled_sweet_potatoes%2F&psig=AOvVaw2wOCtbf7wTc_aMpeG2U8UI&ust=1625555366929000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCOCRn-yvy_ECFQAAAAAdAAAAABAK'
+      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'this test',
+      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
     ),
   ];
   constructor() {}
